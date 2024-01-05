13cf409632fe06e8011885bb61d997ac8f162d7e
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-01-06 00:27:31 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,2 +1,25 @@
-const fname=document.getElementById("#fname");
-console.log(fname)
+const form=document.getElementById("form");
+const submit=document.getElementById("submit");
+
+submit.addEventListener("click", (e) =>{
+
+    e.preventDefault(); //prevent form from submitting
+    const fname=document.getElementById("fname");
+    const lname=document.getElementById("lname");
+    const email=document.getElementById("floatingInput");
+    const address=document.getElementById("floatingTextarea2");
+    const phone=document.getElementById("phone");
+    const password=document.getElementById("floatingPassword");
+    const confirm_password=document.getElementById("confirmPassword");
+
+    if(fname.value.length<5 || lname.value.length <5){
+        alert("First and Last name should be atleast 5 characters long");
+    }
+
+
+
+    const formData = new FormData(form);
+    formData.append(fname,fname.value);
+    const values =[...formData.entries()];
+    console.log(values);
+});
