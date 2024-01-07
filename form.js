c7b8f2afebbdcd6b00a42f31476daf1fed1c6ee0
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-01-07 12:56:17 +0530

    Update form.js

--- form.js
+++ form.js
@@ -1,7 +1,7 @@
 const form=document.getElementById("form");
 const submit=document.getElementById("submit");
 
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded',function(){        //to append country code to the phone number
     const items=document.querySelectorAll(".dropdown-item");
     const phone=document.getElementById("phone");
     items.forEach(function(item){
@@ -14,46 +14,52 @@ document.addEventListener('DOMContentLoaded',function(){
 
 
 
-submit.addEventListener("click", (e) =>{
+submit.addEventListener("click", (e) =>{        //actions taken after clicking on submit
 
     e.preventDefault(); //prevent form from submitting
     const fname=document.getElementById("fname");
     const lname=document.getElementById("lname");
     const email=document.getElementById("floatingInput");
     const address=document.getElementById("floatingTextarea2");
-    
     const password=document.getElementById("floatingPassword");
     const confirm_password=document.getElementById("confirmPassword");
 
     if(fname.value.length<5 || lname.value.length <5){
         alert("First and Last name should be atleast 5 characters long!!");
+        return false;
     }
 
-    if(phone.value.length<13 || phone.value.length>13){
+    if(phone.value.length<13 || phone.value.length>14){
         alert("Phone number should be less than 10!!");
+        return false;
     }
     if(phone.value.substring(3,phone.value.length) == "123456789"){
         alert("Please Enter a different phone number!!");
+        return false;
     }
     
     if(email.value.includes("@") == false){
         alert("Please enter a valid Email ID!!");
+        return false;
     }
 
     if(password.value.includes("password")){
         alert("Password cannot be password!!");
+        return false;
     }
     if(password.value.includes(fname.value+lname.value)){
         alert("Password cannot be same as the username!!");
+        return false;
     } 
     if(password.value.length<8){
         alert("Please pick a stronger password!!");
+        return false;
     }
 
     if(password.value != confirm_password.value){
-        alert("Passwords should match!!");
+        alert("Passwords not matching!!");
+        return false;
     }
 
-    
-
+    return alert("Form Submitted Successfully!!");
 });
